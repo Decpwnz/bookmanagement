f193c1f9994a4487cab25d9b652573f6405d5bd9
Marijus <electromenn@gmail.com>
2024-09-18 13:04:39 +0300

    add books controller to books module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -2,18 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksService } from './books.service';
 import { Book, BookSchema } from '../schemas/book.schema';
+import { BooksController } from './books.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
   ],
+  controllers: [BooksController],
   providers: [BooksService],
   exports: [BooksService],
 })
 export class BooksModule {}
-
-// Book.name is a property provided by the @Schema() decorator from @nestjs/mongoose.
-// It's automatically added to the Book class when we define it using @Schema().
-// In this case, Book.name will be the string 'Book', which is used as the
-// collection name in MongoDB. We import it from '../schemas/book.schema'
-// where the Book class is defined with the @Schema() decorator.
